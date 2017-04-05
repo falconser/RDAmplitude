7e588deeef505cc7da5e450772b171b4d5ce57f2
Sergey Ischuk <falcon.ser@gmail.com>
2017-04-05 22:05:45 +0300

    Add osx target

---
+++ Amplitude-OSX/Amplitude_OSX.m
@@ -0,0 +1,13 @@
+//
+//  Amplitude_OSX.m
+//  Amplitude-OSX
+//
+//  Created by Sergey Falcon on 4/5/17.
+//  Copyright © 2017 Amplitude. All rights reserved.
+//
+
+#import "Amplitude_OSX.h"
+
+@implementation Amplitude_OSX
+
+@end
